0c3f3878b45bd906b0f8a8294eb04aa5dd798416
Chetan Rane (CW) <Chetan.Rane@jda.com>
2021-05-30 13:53:30 +0530

    Added touch support

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { DndProvider, useDrag } from 'react-dnd';
 import Capture from './components/capture';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from "react-dnd-touch-backend"
 
 function App() {
 	return (
-		<DndProvider backend={HTML5Backend}>
+		<DndProvider backend={TouchBackend}>
 			<div className='App'>
 				<header className='App-header'>
 					<Capture />
