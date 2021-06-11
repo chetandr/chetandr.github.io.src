be9e7c8ab862a69ed8f8f15a8253350862f9a4fb
Chetan Rane (CW) <Chetan.Rane@jda.com>
2021-06-11 08:10:27 +0530

    stable version

--- src/components/MediaStream/index.jsx
+++ src/components/MediaStream/index.jsx
@@ -11,6 +11,7 @@ const MediaStream = () => {
 	const debug = true;
 	const videoRef = React.useRef();
 	const videoWrapper = React.useRef();
+	const imageRef = React.useRef();
 
 	const canvasRef = React.useRef();
 	const imageCanvasRef = React.useRef();
@@ -19,9 +20,9 @@ const MediaStream = () => {
 	const [mediaSettings, setMediaSettings] = React.useState({});
 	const [debugData, setDebugData] = React.useState({});
 	const [capturing, setCapturing] = React.useState(false);
-	
+	const [stage, setStage] = React.useState('INIT');
 
-	const renderMedia = React.useCallback(async ()=>{
+	const renderMedia = React.useCallback(async () => {
 		const media = await getMedia();
 		if (videoRef.current) {
 			if ('srcObject' in videoRef.current) {
@@ -32,7 +33,7 @@ const MediaStream = () => {
 			setMediaSettings(media.settings);
 			const video_width = media.settings.width;
 			const video_height = media.settings.height;
-			const to_width = window.innerWidth;
+			const to_width = document.documentElement.clientWidth; //window.innerWidth;
 			const to_height = Math.floor((to_width * video_height) / video_width); //window.innerHeight;
 			const ratio = media.settings.aspectRatio;
 			const scaleRatioY = to_height / video_height;
@@ -59,12 +60,12 @@ const MediaStream = () => {
 			// 	to_x = (to_width - video_width) / 2;
 			// 	to_y = to_x / (scaleRatioY * ratio);
 			// }
-			videoWrapper.current.style.width = `${to_width}px`;
-			videoWrapper.current.style.height = `${to_height}px`;
+			// videoWrapper.current.style.width = `${to_width}px`;
+			// videoWrapper.current.style.height = `${to_height}px`;
 			videoRef.current.width = video_width;
 			videoRef.current.height = video_height;
-			canvasRef.current.width = to_width;
-			canvasRef.current.height = to_height;
+			imageRef.current.width = video_width;
+			imageRef.current.height = video_height;
 
 			videoRef.current.style.transform = `translate(${-videoRef.current.offsetLeft}px, ${-videoRef.current
 				.offsetTop}px) scale(${scaleRatioX}, ${scaleRatioY})`;
@@ -82,7 +83,7 @@ const MediaStream = () => {
 			};
 			setDebugData(newDebugData);
 		}
-	}, [videoRef, videoWrapper, canvasRef, imageCanvasRef])
+	}, [videoRef, videoWrapper, canvasRef, imageCanvasRef]);
 	window.addEventListener('orientationchange', function (event) {
 		renderMedia();
 	});
@@ -91,73 +92,91 @@ const MediaStream = () => {
 	}, [videoRef, videoWrapper, canvasRef, imageCanvasRef]);
 
 	const captureImage = () => {
+		const video_width = mediaSettings.width;
+		const video_height = mediaSettings.height;
+		const to_width = document.documentElement.clientWidth; //window.innerWidth;
+		const to_height = document.documentElement.clientHeight; //Math.floor((to_width * video_height) / video_width); //window.innerHeight;
+		// const ratio = media.settings.aspectRatio;
+		const scaleRatioY = to_height / video_height;
+		const scaleRatioX = to_width / video_width;
 		const ctx = imageCanvasRef.current.getContext('2d');
 		setCapturing(true);
+		videoRef.current.pause();
+		videoRef.current.currentTime = 0;
 		// ctx.scale(scaleRatioX, scaleRatioY);
 		ctx.drawImage(videoRef.current, 0, 0);
 
 		const url = imageCanvasRef.current.toDataURL('image/png');
+		imageRef.current.src = url;
+		videoRef.current.style.display = 'none';
+		imageRef.current.style.display = 'block';
+		imageRef.current.style.transform = `translate(${-videoRef.current.offsetLeft}px, ${-videoRef.current
+			.offsetTop}px) scale(${scaleRatioX}, ${scaleRatioY})`;
+		imageRef.current.style.transformOrigin = 'top left';
+		videoRef.current.style.display = 'none';
+		imageRef.current.style.display = 'block';
 		// photo.current.setAttribute('src', url);
-		downloadAnchor.current.href = url;
-		downloadAnchor.current.download = 'MyPhoto.png';
-		downloadAnchor.current.click();
-		//
-		window.URL.revokeObjectURL(url);
+		// downloadAnchor.current.href = url;
+		// downloadAnchor.current.download = 'MyPhoto.png';
+		// downloadAnchor.current.click();
+		// //
+		// window.URL.revokeObjectURL(url);
 		setTimeout(() => setCapturing(false), 3000);
+		setStage('CAPTURED');
 	};
 
-	const paint = () => {
-		requestAnimationFrame(paint);
-		const ctx = canvasRef.current.getContext('2d');
-		const video_width = mediaSettings.width;
-		const video_height = mediaSettings.height;
-		const to_width = window.innerWidth;
-		const to_height = Math.floor((to_width * video_height) / video_width); //window.innerHeight;
-		var ratio = video_width / video_height;
-		var target_width;
-		var target_height;
-		var y_of_video = 0;
-		var x_of_video = 0;
-		// if (video_width > video_height) {
-		// 	target_width = canvasRef.current.width;
-		// 	target_height = canvasRef.current.width / ratio;
-		// 	y_of_video = (canvasRef.current.height - target_height) / 2;
-		// } else {
-		// 	target_width = canvasRef.current.height;
-		// 	target_height = canvasRef.current.height * ratio;
-		// 	x_of_video = (canvasRef.current.width - target_width) / 2;
-		// }
-		ctx.drawImage(videoRef.current, 0, 0, to_width, to_height);
-	};
-	const metaDataLoadHandle = (e) => {
-		// alert(`${mediaSettings.width}x${mediaSettings.height}`);
-		const newDebugData = {
-			media: `${mediaSettings.width}x${mediaSettings.height}`,
-			video: `${videoRef.current.videoWidth}x${videoRef.current.videoWidth}`,
-			avail: `${window.availWidth}x${window.availHeight}`,
-			window: `${window.innerWidth}x${window.innerHeight}`,
-		};
-		paint();
-		// setDebugData(newDebugData);
-		// setInterval(() => {
-		// 	// console.log(videoRef.current.videoWidth, videoRef.current.videoWidth, window.innerWidth, window.innerHeight);
+	// const paint = () => {
+	// 	requestAnimationFrame(paint);
+	// 	const ctx = canvasRef.current.getContext('2d');
+	// 	const video_width = mediaSettings.width;
+	// 	const video_height = mediaSettings.height;
+	// 	const to_width = window.innerWidth;
+	// 	const to_height = Math.floor((to_width * video_height) / video_width); //window.innerHeight;
+	// 	var ratio = video_width / video_height;
+	// 	var target_width;
+	// 	var target_height;
+	// 	var y_of_video = 0;
+	// 	var x_of_video = 0;
+	// 	// if (video_width > video_height) {
+	// 	// 	target_width = canvasRef.current.width;
+	// 	// 	target_height = canvasRef.current.width / ratio;
+	// 	// 	y_of_video = (canvasRef.current.height - target_height) / 2;
+	// 	// } else {
+	// 	// 	target_width = canvasRef.current.height;
+	// 	// 	target_height = canvasRef.current.height * ratio;
+	// 	// 	x_of_video = (canvasRef.current.width - target_width) / 2;
+	// 	// }
+	// 	ctx.drawImage(videoRef.current, 0, 0, to_width, to_height);
+	// };
+	// const metaDataLoadHandle = (e) => {
+	// 	// alert(`${mediaSettings.width}x${mediaSettings.height}`);
+	// 	const newDebugData = {
+	// 		media: `${mediaSettings.width}x${mediaSettings.height}`,
+	// 		video: `${videoRef.current.videoWidth}x${videoRef.current.videoWidth}`,
+	// 		avail: `${window.availWidth}x${window.availHeight}`,
+	// 		window: `${window.innerWidth}x${window.innerHeight}`,
+	// 	};
+	// 	paint();
+	// 	// setDebugData(newDebugData);
+	// 	// setInterval(() => {
+	// 	// 	// console.log(videoRef.current.videoWidth, videoRef.current.videoWidth, window.innerWidth, window.innerHeight);
 
-		// 	// requestAnimationFrame(() => ctx.drawImage(videoRef.current, x_of_video, y_of_video));
+	// 	// 	// requestAnimationFrame(() => ctx.drawImage(videoRef.current, x_of_video, y_of_video));
 
-		// 	// ctx.drawImage(
-		// 	// 	videoRef.current,
-		// 	// 	0,
-		// 	// 	0,
-		// 	// 	videoRef.current.videoWidth,
-		// 	// 	videoRef.current.videoWidth,
-		// 	// 	0,
-		// 	// 	0,
-		// 	// 	window.innerWidth,
-		// 	// 	window.innerHeight,
-		// 	// );
-		// }, 30);
-		// ctx.drawImage(videoRef.current, 0, 0, mediaSettings.width, mediaSettings.height);
-	};
+	// 	// 	// ctx.drawImage(
+	// 	// 	// 	videoRef.current,
+	// 	// 	// 	0,
+	// 	// 	// 	0,
+	// 	// 	// 	videoRef.current.videoWidth,
+	// 	// 	// 	videoRef.current.videoWidth,
+	// 	// 	// 	0,
+	// 	// 	// 	0,
+	// 	// 	// 	window.innerWidth,
+	// 	// 	// 	window.innerHeight,
+	// 	// 	// );
+	// 	// }, 30);
+	// 	// ctx.drawImage(videoRef.current, 0, 0, mediaSettings.width, mediaSettings.height);
+	// };
 
 	return (
 		<div>
@@ -168,40 +187,49 @@ const MediaStream = () => {
 			)}
 			<div
 				ref={videoWrapper}
-				style={{ marginBottom: '16px', position: 'absolute', top: '0px', left: '0px', display: 'none' }}
+				style={{
+					marginBottom: '16px',
+					position: 'absolute',
+					top: '0px',
+					left: '0px',
+					overflow: 'hidden',
+					width: '100vw',
+					height: '100vh',
+				}}
 			>
+				{/* <div ref={videoWrapper}> */}
 				<video
 					ref={videoRef}
 					id='camvideo'
 					autoPlay
 					playsInline
-					onLoadedMetadata={metaDataLoadHandle}
+					// onLoadedMetadata={metaDataLoadHandle}
 					// style={{ display: 'none' }}
+					// className="videosize"
 				></video>
+				<img ref={imageRef} style={{ border: 'solid 2px red', display: 'none' }}></img>
 			</div>
 
 			<div>
-				<canvas ref={canvasRef} style={{ border: 'solid 2px red' }}></canvas>
+				<canvas ref={canvasRef} style={{ border: 'solid 2px red', display: 'none' }}></canvas>
 				<canvas ref={imageCanvasRef} style={{ border: 'solid 2px red', display: 'none' }}></canvas>
 			</div>
 			<a id='downloadAnchor' href='http://chetandr.github.com' ref={downloadAnchor} style={{ display: 'none' }}>
 				image
 			</a>
 			<div id='changeVideo'>
-				<IconButton
-					color='primary'
-					aria-label='upload picture'
-					component='span'
-					variant='contained'
-					// ref={snap}
-					onClick={captureImage}
-				>
-					{capturing ? (
-						<CircularProgress color='secondary' thinkness={7} size={100} />
-					) : (
+				{stage === 'INIT' && (
+					<IconButton
+						color='primary'
+						aria-label='upload picture'
+						component='span'
+						variant='contained'
+						// ref={snap}
+						onClick={captureImage}
+					>
 						<PhotoCamera style={{ fontSize: 100 }} />
-					)}
-				</IconButton>
+					</IconButton>
+				)}
 			</div>
 			{/* <div id='additionalButtons'>
 				<IconButton
